fdeba890952b8478b82ec3584461827be60cb5fd
techlead-shootorder <158041977+techlead-shootorder@users.noreply.github.com>
2025-04-25 14:05:58 +0530

    Add files via upload

---
+++ hr-outsourcing.php
@@ -0,0 +1,557 @@
+<?php include("main-header.php"); ?>
+
+<title>HR Outsource Services Company in Hyderabad | Human Resources | Payroll Processing - Merakhata</title>
+
+<meta name="description"
+    content="Merakhata.com is a technology driven HR outsourcing services company in Hyderabad, India. We are your trusted partners for ✓ Human Resource Management ✓ Payroll Processing ✓ Statuory Compliance ✓ HR Consultants and much more HR services... Get in touch with us for an in-person meet.  ">
+
+<meta name="keywords"
+    content="HR Services, hr outsoucing services company in hyderabad, hr outsourcing, payroll outsourcing, human resounce management">
+
+
+
+</head>
+
+<?php include("header.php"); ?>
+
+<section class="">
+
+    <div class="desktop-banner">
+
+        <img src="images/banners/hr-outsourcing-banner.png" alt="banner" class="img-fluid" width="100%">
+
+    </div>
+
+    <div class="mobile-banner">
+
+        <img src="images/banners/hr-outsourcing-mobile.png" alt="banner" class="img-fluid" width="100%">
+
+    </div>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-1"></div>
+
+            <div class="col-md-5 banner-text">
+
+                <h2 class="text-black h2">Trusted Partner for</h2>
+
+                <h1 class="text-black h1">All Your HR Needs</h1>
+
+            </div>
+
+            <div class="col-md-1"></div>
+
+            <div class="col-md-5">
+
+                <div class="form-bg shadow-lg">
+
+                    <?php include("banner-form.php"); ?>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="Features padding-50">
+
+    <div class="container">
+
+        <h2>Features</h2>
+
+    </div>
+    <!-- /.container -->
+    <div class="container py-4 features-tabs shadow-lg">
+        <div class="row">
+            <div class="col-md-4">
+                <ul id="tabsJustified" class="nav nav-pills flex-column">
+                    <li class="nav-item"><a href="#human" data-target="#human" data-toggle="tab"
+                            class="nav-link small text-uppercase active"><img
+                                src="images/icons/human-resources-blue.png">&nbsp;HUMAN RESOURCES</a></li>
+                    <li class="nav-item"><a href="#payroll" data-target="#payroll" data-toggle="tab"
+                            class="nav-link small text-uppercase "><img
+                                src="images/icons/payroll-processing-blue.png">&nbsp;PAYROLL PROCESSING</a></li>
+
+                    <li class="nav-item"><a href="#stat" data-target="#stat" data-toggle="tab"
+                            class="nav-link small text-uppercase"><img
+                                src="images/icons/statutory-compliance-blue.png">&nbsp;STATUTORY COMPLIANCES</a></li>
+
+                    <li class="nav-item"><a href="#mis" data-target="#mis" data-toggle="tab"
+                            class="nav-link small text-uppercase"><img
+                                src="images/icons/mis-reporting-blue.png">&nbsp;MIS REPORTING</a></li>
+                </ul>
+            </div>
+            <div class="col-md-8">
+                <div class="tab-content p-3 w-100">
+                    <div id="human" class="tab-pane fade active show">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <ul class="ul">
+                                    <li>Preparation and creation of Company Administrative policy and Employee Policy
+                                    </li>
+
+                                    <li>Preparation and creation of HR Policy</li>
+
+                                    <li>Income tax Planning of Employees</li>
+
+                                    <li>Leave management</li>
+
+                                    <li>Attendance Management</li>
+
+                                    <li>Employee Performance Management</li>
+
+                                    <li>Employee expenses reimbursement policy</li>
+
+                                    <li>Employee Onboarding and Exit process</li>
+
+                                    <li>Employee background verification</li>
+
+                                    <li>HR Consultant Services</li>
+
+
+
+                                </ul>
+
+                            </div>
+
+                            <div class="col-md-6">
+
+                                <img src="images/human-resources.png" class="img-fluid">
+
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+
+                    <div id="payroll" class="tab-pane fade">
+
+                        <div class="row">
+
+                            <div class="col-md-6">
+
+                                <ul class="ul">
+
+                                    <li>Creation of Salary structure</li>
+
+                                    <li>Calculation and processing of monthly salary</li>
+
+                                    <li>Preparation of Monthly Payroll register</li>
+
+                                    <li>Preparation and generation of Payslips</li>
+
+                                    <li>Generation of Form 16 ( both Part A & B)</li>
+
+                                    <li>Different MIS Reports on Employee Compensation</li>
+
+
+
+                                </ul>
+
+                            </div>
+
+                            <div class="col-md-6">
+
+                                <img src="images/payroll-processing.png" class="img-fluid">
+
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+
+                    <div id="stat" class="tab-pane fade">
+
+                        <div class="row">
+
+                            <div class="col-md-6">
+
+                                <ul class="ul">
+
+                                    <li>Calculation of monthly statutory liabilities like PF, ESI, Professional Tax, TDS
+                                    </li>
+
+                                    <li>Preparation of monthly payment challan for Statutory liabilities</li>
+
+                                    <li>Filing of Returns for Different Statutory liabilities</li>
+
+                                    <li>Consultation on Statutory compliances</li>
+
+                                    <li>Registration with Various Statutory bodies</li>
+
+
+
+                                </ul>
+
+                            </div>
+
+                            <div class="col-md-6">
+
+                                <img src="images/statutory-compliance.png" class="img-fluid">
+
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+
+                    <div id="mis" class="tab-pane fade">
+
+                        <div class="row">
+
+                            <div class="col-md-6">
+
+                                <ul class="ul">
+
+                                    <li>Reporting on Salary</li>
+
+                                    <li>Reporting on Employee Leaves balance, accrual and used</li>
+
+                                    <li>Reports on Statutory compliances</li>
+
+                                    <li>Reports on the company assets with employees</li>
+
+                                    <li>Reporting on the defaults by Employees</li>
+
+                                    <li>Reporting on company IT Structure</li>
+
+
+
+                                </ul>
+
+                            </div>
+
+                            <div class="col-md-6">
+
+                                <img src="images/hr-mis-report.png" class="img-fluid">
+
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+
+
+
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<section class="compare-packages padding-50">
+
+    <div class="container">
+
+        <h3 class="text-center">Compare Our Packages</h3>
+
+        <hr class="hr1">
+
+    </div>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-10 my-4 mx-auto">
+
+
+
+                <div class="row">
+
+                    <div class="col-lg-12">
+
+                        <div class="table-responsive">
+
+                            <table class="table table-bordered table-striped table-hover">
+
+                                <thead class="thead-inverse text-center">
+
+                                    <tr>
+
+                                        <th class="border-hide"></th>
+
+                                        <th class="startup">Startup</th>
+
+                                        <th colspan="2" class="smlb">Small, Medium & Large Business</th>
+
+                                    </tr>
+
+                                    <tr>
+
+                                        <th class="border-hide"></th>
+
+                                        <th class="price-info text-center">
+
+                                            <p>Startup</p>
+
+                                            <div class="price-was">₹ 12,000 INR</div>
+
+                                            <div class="price-now"><span>₹ 9,999 <span
+                                                        class="price-small">INR</span></span>
+
+                                            </div>
+
+                                            <button class="btn btn-small bordered">month</button>
+
+                                            <div><a href="#" class="price-buy btn btn-success text-white">Enquire <span
+                                                        class="hide-mobile">Now</span></a></div>
+
+                                        </th>
+
+                                        <th class="price-info text-center">
+
+                                            <p>Medium</p>
+
+                                            <div class="price-was">₹ 25,000 INR</div>
+
+                                            <div class="price-now"><span>₹ 19,999 <span
+                                                        class="price-small">INR</span></span></div>
+
+                                            <div>
+
+                                                <button class="btn btn-small bordered">month</button>
+
+                                            </div>
+
+                                            <div><a href="#" class="price-buy btn btn-success text-white">Enquire <span
+                                                        class="hide-mobile">Now</span></a></div>
+
+                                        </th>
+
+                                        <th class="price-info text-center">
+
+                                            <p>Large</p>
+
+                                            <div class="price-was">₹ 40,000 INR</div>
+
+                                            <div class="price-now"><span>₹ 29,999 <span
+                                                        class="price-small">INR</span></span></div>
+
+                                            <button class="btn btn-small bordered">month</button>
+
+                                            <div><a href="#" class="price-buy btn btn-success text-white">Enquire <span
+                                                        class="hide-mobile">Now</span></a></div>
+
+                                        </th>
+
+
+
+                                    </tr>
+
+                                </thead>
+
+                                <tbody class="text-center">
+
+
+
+
+
+                                    <tr>
+
+                                        <td class="w-25">Preparation and creation of Company HR, Admin and Employee
+                                            Policy</td>
+
+                                        <td class="green"><i class="fa fa-check"></i></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+
+
+                                    </tr>
+
+                                    <tr>
+
+                                        <td class="option">Creation of Salary Structure</td>
+
+                                        <td class="green"><i class="fa fa-check"></i></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+
+
+                                    </tr>
+
+                                    <tr>
+
+                                        <td class="option">Payroll Register</td>
+
+                                        <td class="green"><i class="fa fa-check"></i></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+
+
+                                    </tr>
+
+                                    <tr>
+
+                                        <td class="option">Creation of Payslips</td>
+
+                                        <td class="green"><i class="fa fa-check"></i></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+
+
+                                    </tr>
+
+                                    <tr>
+
+                                        <td class="option">Generation of Form 16</td>
+
+                                        <td class="green"><i class="fa fa-check"></i></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+
+
+                                    </tr>
+
+                                    <tr>
+
+                                        <td class="option">Calculation of monthly statutory liabilities like PF, ESI,
+                                            Professional Tax, TDS</td>
+
+                                        <td class="green"><i class="fa fa-check"></i></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+
+
+                                    </tr>
+
+                                    <tr>
+
+                                        <td class="option">Attendence Management</td>
+
+                                        <td></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+
+
+                                    </tr>
+
+                                    <tr>
+
+                                        <td class="option">Employee Performance Management</td>
+
+                                        <td></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+
+
+                                    </tr>
+
+                                    <tr>
+
+                                        <td>Leave Management</td>
+
+                                        <td></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+
+
+                                    </tr>
+
+                                    <tr>
+
+                                        <td>MIS Reporting on Salary, Leave and Attendence</td>
+
+                                        <td></td>
+
+                                        <td></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+
+
+                                    </tr>
+
+                                    <tr>
+
+                                        <td>Consultation on Statutory compliances</td>
+
+                                        <td></td>
+
+                                        <td></td>
+
+                                        <td class="blue"><i class="fa fa-check"></i></td>
+
+
+
+                                    </tr>
+
+                                    <tr>
+
+                                        <td>Employees</td>
+
+                                        <td>25</td>
+
+                                        <td>50</td>
+
+                                        <td>100</td>
+
+
+
+                                    </tr>
+
+                                </tbody>
+
+
+
+                            </table>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+<?php include("main-footer.php"); ?>
