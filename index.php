b20c3851e494a50a10766c74858aa4328b4d55c6
techlead-shootorder <158041977+techlead-shootorder@users.noreply.github.com>
2025-04-25 11:40:19 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,184 @@
+<?php include("main-header.php"); ?>
+<title>Accounting Services Company in Hyderabad, India - Merakhata</title>
+<meta name="description"
+    content="Merakhata.com is  a technology driven accounting services company in Hyderabad helps providing the business solutions to the business at the same time educate and help the Individuals in filling income tax return and advise them in financial planning and tax saving.">
+<meta name="keywords"
+    content="merakhata, accounting services company in hyderabad, accounting services, taxation services">
+</head>
+<?php include("header.php"); ?>
+<section class="services">
+    <div class="desktop-banner">
+        <img src="images/banners/home.png" alt="banner" class="img-fluid" width="100%">
+    </div>
+    <div class="mobile-banner">
+        <img src="images/banners/home-mobile.png" alt="banner" class="img-fluid" width="100%">
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 banner-text">
+                <img src="images/green.png" class="img-fluid pb-3"> &nbsp;<h1 class="head text-white d-inline">Debit
+                    Your Business Troubles</h1><br />
+                <img src="images/green.png" class="img-fluid  pb-3"> &nbsp; <span class="head text-white">Credit
+                    MERAKHATA Services</span>
+                <p class="pt-3 text-white">Trusted Partner for all your </p>
+                <h6 class="text-white">Accounting, HR & Tax Services</h6>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-5">
+                <div class="form-bg shadow-lg">
+                    <?php include("banner-form.php"); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container padding-50">
+        <h2>WHY US ?</h2>
+        <p class="pt-4"><b>Merakhata.com</b> is more than just another accounting or HR outsourcing company. Being a
+            startup ourselves, we understand the need of a startup entrepreneur. We are a one-stop shop which not only
+            provides the Accounting, HR and Tax services but we also are affiliated with one of the best digital
+            marketing company in India which help them advertising your product. Our business model is such that more
+            emphasis has been given on the reporting and presentation of different results to the business which is
+            essential for the survival and success of the company. In a nutshell, we believe in your success.</p>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="service-img text-center">
+                    <img src="images/data-security.png" alt="data security" class="img-fluid">
+                    <h4>Data security</h4>
+                    <p>Data integrity is our priority. With the help of an inbuilt team of IT Professionals who have
+                        developed different software for us, we assure that data security is not compromised.</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-img text-center">
+                    <img src="images/dedicated-service.png" alt="dedicated services" class="img-fluid">
+                    <h4>Dedicated Services</h4>
+                    <p>We have a team of talented Chartered Accountants dedicated to providing services to you. With the
+                        emergence of GST, the need for proactive accounting and updating daily bookkeeping is a must. We
+                        help businesses to comply with all the monthly regulatory requirement..</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-img text-center">
+                    <img src="images/safe.png" alt="confidential" class="img-fluid">
+                    <h4>Confidential</h4>
+                    <p>We never sell or share your data outside of Merakhata. It is our principal policy to maintain the
+                        secrecy of your data and material</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="container padding-50">
+    <h2 class="text-center">OUR SERVICES</h2>
+    <!--<p class="text-center">Lorem ipsum dolor sit amet, te duo magna doming. Duo an ferri mundi latine, ut usu ridens menandri. <br/>At simul elitr nostrum cum, eos ullum insolens salutandi ea.</p>-->
+    <div class="mt-5 mb-5">
+        <ul class="nav nav-pills mb-3 justify-content-center" id="pills-tab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <a class="nav-link active" id="pills-acc-tab" data-toggle="pill" href="#pills-acc" role="tab"
+                    aria-controls="pills-acc" aria-selected="true">ACCOUNTS OUTSOURCING</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link" id="pills-hr-tab" data-toggle="pill" href="#pills-hr" role="tab"
+                    aria-controls="pills-hr" aria-selected="false">HR OUTSOURCING</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link" id="pills-tax-tab" data-toggle="pill" href="#pills-tax" role="tab"
+                    aria-controls="pills-tax" aria-selected="false">TAX PRACTICE</a>
+            </li>
+        </ul>
+        <div class="tab-content" id="pills-tabContent">
+            <div class="tab-pane fade show active" id="pills-acc" role="tabpanel" aria-labelledby="pills-acc-tab">
+                <div class="row">
+                    <div class="col-md-6 mt-5">
+                        <div class="d-flex justify-content-center sp">
+                            <ul style="list-style-type:none; padding-left:0px;">
+                                <li><i class="fa fa-long-arrow-right"></i> Bookkeeping</li>
+                                <li><i class="fa fa-long-arrow-right"></i> Reporting</li>
+                                <li><i class="fa fa-long-arrow-right"></i> Consultancy</li>
+                                <li><i class="fa fa-long-arrow-right"></i> Co-ordination with Auditor</li>
+                                <li><i class="fa fa-long-arrow-right"></i> Mis Reports</li>
+                                <li><i class="fa fa-long-arrow-right"></i> Tax Calculation</li>
+                            </ul>
+                        </div>
+
+                    </div>
+                    <div class="col-md-6">
+                        <div class="ser-img">
+                            <img src="images/accounts.png" alt="accounts" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4">
+                        <div class="read-btn">
+                            <a href=""><button type="button" class="btn btn-blue">Read More</button></a>
+                        </div>
+                    </div>
+                    <div class="col-md-4"></div>
+
+                </div>
+            </div>
+            <div class="tab-pane fade" id="pills-hr" role="tabpanel" aria-labelledby="pills-hr-tab">
+                <div class="row">
+                    <div class="col-md-6 mt-5">
+                        <div class="d-flex justify-content-center sp">
+                            <ul style="list-style-type:none; padding-left:0px;">
+                                <li><i class="fa fa-long-arrow-right"></i> Human Resources</li>
+                                <li><i class="fa fa-long-arrow-right"></i> Payroll Processing</li>
+                                <li><i class="fa fa-long-arrow-right"></i> Statutory Compliances</li>
+                                <li><i class="fa fa-long-arrow-right"></i> Mis Reporting</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="ser-img">
+                            <img src="images/hr.png" alt="hr" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4">
+                        <div class="read-btn">
+                            <a href=""><button type="button" class="btn btn-blue">Read More</button></a>
+                        </div>
+                    </div>
+                    <div class="col-md-4"></div>
+
+                </div>
+            </div>
+            <div class="tab-pane fade" id="pills-tax" role="tabpanel" aria-labelledby="pills-tax-tab">
+                <div class="row">
+                    <div class="col-md-6 mt-5">
+                        <div class="d-flex justify-content-center sp">
+                            <ul style="list-style-type:none; padding-left:0px;">
+                                <li><i class="fa fa-long-arrow-right"></i> GST Services</li>
+                                <li><i class="fa fa-long-arrow-right"></i> TDS Services</li>
+                                <li><i class="fa fa-long-arrow-right"></i> ITR</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="ser-img">
+                            <img src="images/tax.png" alt="tax" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4"></div>
+                    <div class="col-md-4">
+                        <div class="read-btn">
+                            <a href=""><button type="button" class="btn btn-blue">Read More</button></a>
+                        </div>
+                    </div>
+                    <div class="col-md-4"></div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include("main-footer.php"); ?>
