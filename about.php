02f203c4dc661b301b29588e5d3240cadb7b7e50
techlead-shootorder <158041977+techlead-shootorder@users.noreply.github.com>
2025-04-25 12:17:58 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,87 @@
+<?php include("main-header.php");?>
+<title>About Us - Merakhata</title>
+<meta name="description" content="Merakhata.com is a technology driven service portal helps providing the business solutions to the business at the same time educate and help the Individuals in filling income tax return and advise them in financial planning and tax saving.">
+<meta name="keywords" content="merakhata">
+        
+</head>
+<?php include("header.php");?>
+        <section class="about pt-5">
+            
+            <div class="container">
+                <h2>About Us</h2>
+                <div class="row">
+                    
+                    <div class="col-md-6">
+	                <p class="pt-4">We are a team of young Chartered accountant worked for about 7-10 years with Big 4s and other major audit firms having vast experience in Accounting, Auditing and tax. Merakhata.com is a technology driven service portal helps providing the business solutions to the business at the same time educate and help the Individuals in filling income tax return and advise them in financial planning and tax saving.</p>
+	                <p>We provide end to end solutions to the startups in terms of providing them the Accounting outsource services, 
+	                HR outsource services, Tax compliances (including Income tax, TDS and GST) and Financial Reporting compliances.</p>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="images/about.png" alt="" class="img-fluid">
+                    </div>
+	                <div class="col-md-12">
+	                    <p> We also provide the financial services to the business by helping them getting Bank Finance. We believe in Value addition and as such we don’t restrict ourselves to be an external agency working on the agreed upon services, However using our expertise, we provide different analytical reports and graphical presentation to the business which is critical to their success and growth.</p>
+                        <p>Date integrity is out top most priority and with the help of inbuilt team of IT Professional who has developed the different software for us which insures that data is not compromised.</p>
+	                </div>
+	            </div>
+	       </div>
+        </section>
+      
+        <section class="rate">
+            <div class="container">
+                <div class="row text-center">
+                    <div class="col-md-3 ">
+                        <img src="images/accu.png" alt="accurate" class="img-fluid" width="50%">
+                        <h2 class="white-text">100%</h2>
+                        <p class="white-text">Accurate and <br/>Timely Reporting</p>
+                    </div>
+                    <div class="col-md-3">
+                        <img src="images/real.png" alt="real" class="img-fluid" width="50%">
+                        <h2 class="white-text">Realtime</h2>
+                        <p class="white-text">Professional <br/>Consultancy</p>
+                    </div>
+                    <div class="col-md-3">
+                        <img src="images/growth.png" alt="growth" class="img-fluid" width="50%">
+                        <h2 class="white-text">100%</h2>
+                        <p class="white-text">Partner in <br/>your growth</p>
+                    </div>
+                    <div class="col-md-3">
+                        <img src="images/mis.png" alt="" class="img-fluid" width="50%">
+                        <h2 class="white-text">Guaranteed</h2>
+                        <p class="white-text">Monthly <br/>MIS reporting</p>
+                    </div>
+                </div>
+            </div>            
+        </section>
+
+        <section class="services">
+                        <div class="container padding-50">
+	            <h2>WHY US ?</h2>
+	            <p class="pt-4"><b>Merakhata.com</b> is more than just another accounting or HR outsourcing company. Being a startup ourselves, we understand the need of a startup entrepreneur. We are a one-stop shop which not only provides the Accounting, HR and Tax services but we also are affiliated with one of the best digital marketing company in India which help them advertising your product. Our business model is such that more emphasis has been given on the reporting and presentation of different results to the business which is essential for the survival and success of the company. In a nutshell, we believe in your success.</p>
+	            <div class="row">
+	                <div class="col-md-4">
+	                    <div class="service-img text-center">
+	                        <img src="images/data-security.png" alt="" class="img-fluid">
+	                        <h4>Data security</h4>
+	                        <p>Data integrity is our priority. With the help of an inbuilt team of IT Professionals who have developed different software for us, we assure that data security is not compromised.</p>
+	                    </div>
+	                </div>
+	               <div class="col-md-4">
+	                    <div class="service-img text-center">
+	                        <img src="images/dedicated-service.png" alt="" class="img-fluid">
+	                        <h4>Dedicated Services</h4>
+	                        <p>We have a team of talented Chartered Accountants dedicated to providing services to you. With the emergence of GST, the need for proactive accounting and updating daily bookkeeping is a must. We help businesses to comply with all the monthly regulatory requirement..</p>
+	                    </div>
+	                </div>
+	                <div class="col-md-4">
+	                    <div class="service-img text-center">
+	                        <img src="images/safe.png" alt="" class="img-fluid">
+	                        <h4>Confidential</h4>
+	                        <p>We never sell or share your data outside of Merakhata. It is our principal policy to maintain the secrecy of your data and material</p>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+        </section>
+        </section>
+        <?php include("footer.php");?>
