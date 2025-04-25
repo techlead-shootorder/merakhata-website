ac66b8a8c3ac6373b8a8c1042531155fc12dfb99
techlead-shootorder <158041977+techlead-shootorder@users.noreply.github.com>
2025-04-25 15:28:47 +0530

    Add files via upload

---
+++ qa/pipe.php
@@ -0,0 +1,36 @@
+#!/usr/bin/php -q
+<?php
+
+use App\Services\Mail\IncomingMailHandler;
+use App\Services\Mail\Transformers\MimeMailTransformer;
+
+ini_set('memory_limit', '256M'); //The concern here is having enough mem for emails with attachments.
+
+if ( ! defined('STDIN')) {
+    define('STDIN', fopen('php://stdin', 'r'));
+}
+
+$content = '';
+while($f = fgets(STDIN)){
+    $content .= $f;
+}
+
+define('PATH_INSTALL', str_replace("\\", "/", dirname(__FILE__)));
+
+$autoloadFile = PATH_INSTALL . '/vendor/autoload.php';
+if (!file_exists($autoloadFile)) {
+    throw new Exception('Unable to find autoloader: ~/vendor/autoload.php');
+}
+require $autoloadFile;
+
+$appFile = PATH_INSTALL . '/bootstrap/app.php';
+if (!file_exists($appFile)) {
+    throw new Exception('Unable to find app loader: ~/bootstrap/app.php');
+}
+$app = require_once $appFile;
+$kernel = $app->make('Illuminate\Contracts\Console\Kernel');
+$kernel->bootstrap();
+
+app(IncomingMailHandler::class)
+    ->parseEmailIntoTicketOrReply($content, MimeMailTransformer::class);
+?>
