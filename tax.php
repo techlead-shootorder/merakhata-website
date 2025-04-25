66746ca9422ca6175c22cddd3ca0688028f2960d
techlead-shootorder <158041977+techlead-shootorder@users.noreply.github.com>
2025-04-25 14:07:56 +0530

    Add files via upload

---
+++ tax.php
@@ -0,0 +1,139 @@
+<?php include("main-header.php");?>
+<title>TAX Consultants in Hyderabad, India | GST | TDS Services - Merakhata</title>
+<meta name="description" content="Merakhata.com is a technology driven TAX consultant in Hyderabad, India. We are your trusted partners for ✓ GST Services, ✓ TDS Compliances, ✓ GST Consultancy. Get in touch with us for an in-person meet.">
+<meta name="keywords" content="tax consultant in hyderabad, gst services, tds services, return filing, gst filing, gst consultant company in hyderabad">
+        
+</head>
+<?php include("header.php");?>
+        <section class="services">
+            <div class="desktop-banner">
+                <img src="images/banners/tax-practice-banner.png" alt="banner" class="img-fluid" width="100%">
+            </div>
+            <div class="mobile-banner">
+                <img src="images/banners/tax-practice-mobile.png" alt="banner" class="img-fluid" width="100%">
+            </div>
+            <div class="container">
+	            <div class="row">
+    	            
+    	            <div class="col-md-6 banner-text">
+    	                <h2 class="text-black h2" >Trusted Partner for</h2>
+                        <h1 class="text-black h1">All The Tax Practices</h1>
+    	            </div>
+    	            <div class="col-md-1"></div>
+    	            <div class="col-md-5">
+                        <div class="form-bg shadow-lg">
+                            <?php include("banner-form.php");?>
+                        </div>
+                    </div>
+            </div>
+            </div>
+            <div class="container padding-50">
+	            <h2>WHY US ?</h2>
+	            <p class="pt-4"><b>Merakhata.com</b> is more than just another accounting or HR outsourcing company. Being a startup ourselves, we understand the need of a startup entrepreneur. We are a one-stop shop which not only provides the Accounting, HR and Tax services but we also are affiliated with one of the best digital marketing company in India which help them advertising your product. Our business model is such that more emphasis has been given on the reporting and presentation of different results to the business which is essential for the survival and success of the company. In a nutshell, we believe in your success.</p>
+	            <div class="row">
+	                <div class="col-md-4">
+	                    <div class="service-img text-center">
+	                        <img src="images/data-security.png" alt="" class="img-fluid">
+	                        <h4>Data security</h4>
+	                        <p>Data integrity is our priority. With the help of an inbuilt team of IT Professionals who have developed different software for us, we assure that data security is not compromised.</p>
+	                    </div>
+	                </div>
+	               <div class="col-md-4">
+	                    <div class="service-img text-center">
+	                        <img src="images/dedicated-service.png" alt="" class="img-fluid">
+	                        <h4>Dedicated Services</h4>
+	                        <p>We have a team of talented Chartered Accountants dedicated to providing services to you. With the emergence of GST, the need for proactive accounting and updating daily bookkeeping is a must. We help businesses to comply with all the monthly regulatory requirement..</p>
+	                    </div>
+	                </div>
+	                <div class="col-md-4">
+	                    <div class="service-img text-center">
+	                        <img src="images/safe.png" alt="" class="img-fluid">
+	                        <h4>Confidential</h4>
+	                        <p>We never sell or share your data outside of Merakhata. It is our principal policy to maintain the secrecy of your data and material</p>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+        </section>
+        <section class="Features padding-50">
+            <div class="container">
+                <h2>Features</h2>
+                
+            </div>
+                <!-- /.container -->
+            <div class="container py-4 features-tabs shadow-lg">
+                <div class="row">
+                    <div class="col-md-4">
+                        
+                            <ul id="tabsJustified" class="nav nav-pills flex-column">
+                                <li class="nav-item"><a href="#gst" data-target="#human" data-toggle="tab" class="nav-link small text-uppercase"><span><img src="images/icons/gst-blue.png">&nbsp;GST SERVICES</a></li>
+                                <li class="nav-item"><a href="#tds" data-target="#payroll" data-toggle="tab" class="nav-link small text-uppercase "><img src="images/icons/tds-blue.png">&nbsp;TDS COMPLIANCES</a></li>
+                                
+                            </ul>
+                    </div>
+                    <div class="col-md-8">
+                            <div class="tab-content p-3 w-100">
+                                <div id="gst" class="tab-pane fade">
+                                   <div class="row">
+                                       <div class="col-md-6">
+                                           <ul class="ul">
+                                      <li> GST Registration</li>
+                                        <li>GST accounting and consultation</li>
+                                        <li>Preparation and Filling of all the GST form</li>
+                                        <li>GST Invoicing</li>
+
+                                    </ul>
+                                       </div>
+                                       <div class="col-md-6">
+                                           <img src="images/gst.png" width="50%" class="img-fluid">
+                                       </div>
+                                   </div>
+                                   
+                                    <p>A Business must register for GST if it satisfies any of the following conditions:</p>
+                                    <ul class="ul1">
+                                    <li>If the aggregate Turnover from all the     business activity in the Financial year exceed Rs. 20 Lakh (Rs. 10 Lakh for North Eastern States)</li>
+                                    <li> Making the supply or providing the services to other state</li>
+                                    <li> Having multiple branches or branches in multiple states</li>
+                                    <li> Require to pay GST under reverse charge</li>
+                                    <li> Agent of supplier</li>
+                                    <li> E -Commerce Companies or an aggregator</li>
+                                    <li> Supplies goods or service through E-commerce</li>
+                                    </ul>
+                                    <a href="" class="btn btn-success text-white">Enquire Now</a>
+                                </div>
+                                <div id="tds" class="tab-pane fade">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <ul class="ul">
+                                       <li>TDS Consultation</li>
+                                        <li>Preparation and issuance of Form 16 A & 16</li>
+                                        <li>Form 26AS reconciliation</li>
+                                        <li>Preparation and Filing of TDS Challan</li>
+                                        <li>Prepration and Filing of TDS Returns</li>
+
+                                    </ul>
+                                        </div>
+                                         <div class="col-md-6">
+                                             <img src="images/tds.png" width='50%' class="img-fluid">
+                                         </div>
+                                    </div>
+                                    
+                                   
+                                    <p>TDS means Tax Deduced at Source i.e. persons making payments are required to deduct tax at source at defined slab rates prescribed by the Income Tax Act. In the current scenario, TDS is almost applicable on the any payment the business make. If You pay rent you are liable to deduct TDS. If you enter into contract, you are liable to deduct TDS from the payment you make. You hire a professional, you are liable to deduct TDS. Eventually applicability of TDS is on almost every payment you make. Businesses generally faces two challenges while dealing with TDS.</p>
+                                    <ol>
+                                         <li>when and what amount to deduct as TDS and</li>
+                                         <li>Under Which head TDS to be deduct</li>
+                                    </ol>
+                                    <p>Merakhata helps answering both these question by proactively providing the update on recent changes in the laws to the businesses and taking the training session of the company staff to cope with all the changes in the law. This will help the companies in facilitation of correct payment. Also, Since we work on real time basis, Our team reviews the daily transactions and communicate the business if there is TDS clauses attract on any transaction.</p>
+                                    <a href="" class="btn btn-success text-white">Enquire Now</a>
+                                </div>
+                               
+                                
+                            </div>
+                       
+                    </div>
+                </div>
+            </div>
+        </section>
+       
+        <?php include("main-footer.php");?>
